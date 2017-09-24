4a9b0b8ca074e9b760295c9de0921b505ea187cf
Gharbi Mohammed <mgharbik@gmail.com>
2017-09-24 16:16:55 +0200

    setting up babel transpiler

--- webpack.config.js
+++ webpack.config.js
@@ -5,13 +5,15 @@ module.exports = {
     filename: "bundle.js"
   },
   module: {
-    rules: [
+    loaders: [
+      { test: /\.css$/, loader: "style!css" },
       {
-        test: /\.css$/,
-        use: [
-          "style-loader",
-          "css-loader"
-        ]
+        test: /\.(js|jsx)$/,
+        loader: "babel" ,
+        query: {
+          presets: ["es2015", "stage-0", "react"],
+          plugins: ["transform-runtime"]
+        }
       }
     ]
   }
